e98afc2d997087d3120eb42b433881ffa93f60d8
Juan Pablo Jacob <jacobjuampe@gmail.com>
2018-04-27 21:56:53 -0300

    update tests

--- app/src/test/java/com/unex/expenses/HelperTest.kt
+++ app/src/test/java/com/unex/expenses/HelperTest.kt
@@ -1,17 +1,22 @@
 package com.unex.expenses
 
+import com.unex.expenses.models.Helper
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
+class HelperTest {
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun getOneWeekAgoTimestamp() {
+        val date = Helper.createDate(2010, 9, 20)
+        val timestamp = Helper.getOneWeekAgoTimestamp(date)
+        assertEquals(1286938800000L, timestamp)
+    }
+
+    @Test
+    fun getOneMonthAgoTimestamp() {
+        val date = Helper.createDate(2010, 9, 20)
+        val timestamp = Helper.getOneMonthAgoTimestamp(date)
+        assertEquals(1284951600000, timestamp)
     }
 }
