27a7806b696f1b2475f2b964136de1475f07658a
Juan Pablo Jacob <jacobjuampe@gmail.com>
2018-04-28 10:18:40 -0300

    add settings ui (no activity)

--- app/src/main/java/com/unex/expenses/ui/MainActivity.kt
+++ app/src/main/java/com/unex/expenses/ui/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.unex.expenses.ui
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.unex.expenses.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,5 +19,20 @@ class MainActivity : AppCompatActivity() {
                 .beginTransaction()
                 .replace(R.id.container, SpendingListFragment())
                 .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settings -> {
+                Snackbar.make(container, R.string.menu_settings, Snackbar.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
